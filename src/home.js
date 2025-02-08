ab8ce12ee166f2f890a87b1bf6caa1328afc7f7f
collins-dev1 <collinsxuccessful39@gmail.com>
2025-02-09 00:15:22 +0100

    usestate update

--- src/home.js
+++ src/home.js
@@ -1,7 +1,26 @@
+import { useState } from 'react';
+
 const Home = () => {
+
+    // let name = "Collins"
+    const [name, setName] = useState('Collins');
+    const [age, setAge] = useState(25);
+
+    const handleClick = () => {
+        setName('Somto');
+        setAge(34);
+    }
+
+    // const handleClickAgain = (name, e) => {
+    //     console.log('Hello ' + name, e.target);
+    // }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name} is {age} years old</p>
+            <button onClick={handleClick}>Click me</button>
+            {/* <button onClick={(e) => handleClickAgain('Collins', e)}>Click me again</button> */}
         </div>
      );
 }
--- src/navbar.js
+++ src/navbar.js
@@ -4,7 +4,11 @@ const Navbar = () => {
             <h1>The OgCollins Blog</h1>
             <div className="links">
                <a href="/">Home</a>
-               <a href="/create">New Blog</a>
+               <a href="/create" style={{
+                color: "white",
+                backgroundColor: "#f13544",
+                borderRadius: "10px"
+               }}>New Blog</a>
             </div>
         </nav>
      );
