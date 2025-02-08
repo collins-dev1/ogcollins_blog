1653dca00741056bea3504cda97e1a59d2f0430d
collins-dev1 <collinsxuccessful39@gmail.com>
2025-02-08 00:35:15 +0000

    third commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 
 function App() {
+  const title = 'Welcome to the new blog';
+  const like = 50;
+  const link = "https://facebook.com";
+  //const person = {name: 'collins', age: 40};
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{title}</h1>
+        <p>Liked { like } times</p>
+
+        {/* <p>{person}</p> */}
+        <p>{10}</p>
+        <p>{"Hello, Everyone"}</p>
+        <p>{[1,2,3,4,5]}</p>
+        <p>{Math.random() * 20}</p>
+
+        <a href="https://www.google.com">Google Site</a>
+        <br></br>
+        <a href={link}>Facebook Site</a>
       </div>
     </div>
   );
