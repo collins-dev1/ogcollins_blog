5db61a9b6ecba272dc9b2e33d2104fba8b95d55c
collins-dev1 <collinsxuccessful39@gmail.com>
2025-02-10 23:14:19 +0100

    seventh commit

--- src/home.js
+++ src/home.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import BlogList from './bloglist';
 
 const Home = () => {
 
@@ -32,12 +33,7 @@ const Home = () => {
 
     return(
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written by {blog.author}</p>
-                </div>
-            ))}
+            <BlogList blogs={blogs} title="All Blogs!"/>
         </div>
     );
 }
