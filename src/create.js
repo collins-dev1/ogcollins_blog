1a4ca96b9df353a1efb48141a2b81b79125fb41b
collins-dev1 <collinsxuccessful39@gmail.com>
2025-02-19 00:38:29 +0100

    handlesubmit

--- src/create.js
+++ src/create.js
@@ -4,10 +4,15 @@ const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('collins');
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        const blog = {title, body, author};
+        console.log(blog);
+    } 
     return ( 
         <div className="create">
             <h2>Create a New Blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Blog Title:</label>
                 <input 
                    type="text"
@@ -30,9 +35,9 @@ const Create = () => {
                     <option value="somto">Somto</option>
                 </select>
                 <button>Add Blog</button>
-                <p>{title}</p>
+                {/* <p>{title}</p>
                 <p>{body}</p>
-                <p>{author}</p>
+                <p>{author}</p> */}
             </form>
         </div>
      );
--- src/navbar.js
+++ src/navbar.js
@@ -2,19 +2,21 @@ import { Link } from "react-router-dom";
 
 
 const Navbar = () => {
-    return ( 
+    return (
         <nav className="navbar">
-            <h1>The OgCollins Blog</h1>
+            <Link to="/">
+                <h1>The OgCollins Blog</h1>
+            </Link>
             <div className="links">
-               <Link to="/">Home</Link>
-               <Link to="/create" style={{
-                color: "white",
-                backgroundColor: "#f13544",
-                borderRadius: "10px"
-               }}>New Blog</Link>
+                <Link to="/">Home</Link>
+                <Link to="/create" style={{
+                    color: "white",
+                    backgroundColor: "#f13544",
+                    borderRadius: "10px"
+                }}>New Blog</Link>
             </div>
         </nav>
-     );
+    );
 }
- 
+
 export default Navbar;
