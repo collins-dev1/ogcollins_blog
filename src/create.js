98ab1a79bd9cafffc14ec9d899d4c5da1ccbd5f7
collins-dev1 <collinsxuccessful39@gmail.com>
2025-02-19 23:56:22 +0100

    adding blog

--- src/create.js
+++ src/create.js
@@ -4,10 +4,23 @@ const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('collins');
+    const [isPending, setIsPending] = useState(false);
+
+
     const handleSubmit = (e) =>{
         e.preventDefault();
         const blog = {title, body, author};
-        console.log(blog);
+
+        setIsPending(true);
+        
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log('new blog added');
+            setIsPending(false);
+        })
     } 
     return ( 
         <div className="create">
@@ -34,7 +47,8 @@ const Create = () => {
                     <option value="collins">Collins</option>
                     <option value="somto">Somto</option>
                 </select>
-                <button>Add Blog</button>
+                {!isPending && <button>Add Blog</button>}
+                {isPending && <button>Blog adding.....</button>}
                 {/* <p>{title}</p>
                 <p>{body}</p>
                 <p>{author}</p> */}
